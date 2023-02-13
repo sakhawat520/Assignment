9ec23caace3bd16bbaec677e7ccd3434a1a261c4
sakhawat520 <sakhawat520378@gmail.com>
2023-02-13 17:22:26 +0600

    first commit

--- index1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-$sum = 0;
-$result = ($sum > 0) ? "$result = Positive" : (($sum < 0) && ($sum < -10) ? $result = "value is below -10" : $result = "negative");
-echo $result;
--- indext2.php
+++
@@ -1,4 +0,0 @@
-<?php
-$sum = -80;
-$result = ($sum > 0) ? "$result = Positive" : (($sum < -10)? $result = " value is below -10" :(($sum < 0) ?  $result = "negative" :$result = "It Zero") );
-echo $result;
